937725cc94bdfa77524db6c716d2429e104b3d37
Alan Hale III <alanhaleiii@gmail.com>
2021-12-22 15:57:17 -0500

    Add pwsh module documentation

--- pwsh/Microsoft.PowerShell_profile.ps1
+++ pwsh/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,11 @@
+# oh-my-posh setup
+# https://ohmyposh.dev/docs/windows
+# install: Install-Module oh-my-posh -Scope CurrentUser
+# update: Update-Module oh-my-posh
 Import-Module oh-my-posh
 Set-PoshPrompt -Theme ah3
+
+# terminal icons setup
+# https://github.com/devblackops/Terminal-Icons
+# install: Install-Module -Name Terminal-Icons -Repository PSGallery
 Import-Module -Name Terminal-Icons
